df2d548b4d8e5891938f51a30773a63c55b8a7de
ainmohamad <47972834+ainmohamad@users.noreply.github.com>
2020-06-22 08:29:31 +0800

    Update editprofile.blade.php

--- resources/views/pages/editprofile.blade.php
+++ resources/views/pages/editprofile.blade.php
@@ -1,4 +1,4 @@
-  
+
 <link rel="stylesheet" type="text/css" href="css/main.css">
 
 @extends('layouts.app')
@@ -6,7 +6,12 @@
 
 @section('content')
 
+<br>
 <div class="container">
+    <br>
+    <center>
+        <div><img src="/storage/{{$user->profile->avatar}}" ></div>
+    </center>
     <div class="row my-2">
         <div class="col-lg-8 order-lg-2">
             
@@ -20,49 +25,25 @@
             <div class="tab-content py-4">
                 <div class="tab-pane active" id="profile">
                     <h5 class="mb-3">{{ __('Personal Information ')}}</h5>
-                    <form method="POST" action="{{ route('store') }}" enctype="multipart/form-data">
-                        {{ csrf_field() }}
+                    <form method="post" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+                        @csrf
+                        @method('PATCH')
 
-                        @if(session('success'))
+                        @if(Session::has('message'))
                             <div class="alert alert-success" roles="alert">
-                            {{ session('success')}}
+                            {{ Session::get('message') }}
                             </div>
                         @endif
 
-                        
-                        <div class="form-group row">
-                            <label for="matric_no" class="col-lg-3 col-form-label form-control-label">{{ __('Name:') }}</label>
-
-                            <div class="col-lg-9">
-                                <label type="textarea" id="name" type="name" class="form-control @error('name') is-invalid @enderror" name="name" > {{ Auth::user()->name }}</label>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-lg-3 col-form-label form-control-label">{{ __('Email') }}</label>
-
-                            <div class="col-lg-9">
-                                <label type="textarea" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email"> {{ Auth::user()->email }}</label>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                        <div class="card-body">
+                            
 
                         <div class="form-group row">
                             <label for="matric_no" class="col-lg-3 col-form-label form-control-label">{{ __('Matric No.') }}</label>
 
                             <div class="col-lg-9">
-                                <input type="textarea" id="matric" class="form-control @error('matric') is-invalid @enderror" name="matric" >
+                                <input type="textarea" id="matric" class="form-control @error('matric') is-invalid @enderror" name="matric"
+                                value="{{ old('matric') ?? $user->profile->matric  }}">
 
                                 @error('matric_no')
                                     <span class="invalid-feedback" role="alert">
@@ -76,11 +57,8 @@
                             <label for="kulliyyah" class="col-lg-3 col-form-label form-control-label">{{ __('Kulliyyah') }}</label>
 
                             <div class="col-lg-9">
-                                <select id="kulliyyah" class="form-control @error('kulliyyah') is-invalid @enderror" name="kulliyyah">
-                                  <option id="koed">Kulliyyah of Education</option>
-                                  <option id="kict">Kulliyyah of Information Technology</option>
-
-                                </select> 
+                                <input type="textarea" id="kulliyyah" class="form-control @error('kulliyyah') is-invalid @enderror" name="kulliyyah" 
+                                value="{{ old('kulliyyah') ?? $user->profile->kulliyyah }}"> 
 
                                 @error('kulliyyah')
                                     <span class="invalid-feedback" role="alert">
@@ -94,7 +72,9 @@
                             <label for="level" class="col-lg-3 col-form-label form-control-label">{{ __('Level') }}</label>
 
                             <div class="col-lg-9">
-                                <select class="form-control" id="level" type="level" class="form-control @error('level') is-invalid @enderror" name="level">
+                                <select class="form-control" id="level" type="level" class="form-control @error('level') is-invalid @enderror" name="level"
+                                value="{{ old('level') ?? $user->profile->level }}">
+
                                   <option id="1">1</option>
                                   <option id="2">2</option>
                                   <option id="3">3</option>
@@ -114,7 +94,8 @@
                             <label for="matric_no" class="col-lg-3 col-form-label form-control-label">{{ __('Phone No.') }}</label>
 
                             <div class="col-lg-9">
-                                <input id="phone" type="textarea" class="form-control @error('phone') is-invalid @enderror" name="phone" >
+                                <input id="phone" type="textarea" class="form-control @error('phone') is-invalid @enderror" name="phone" 
+                                value="{{ old('phone') ?? $user->profile->phone }}">
 
                                 @error('phone')
                                     <span class="invalid-feedback" role="alert">
@@ -128,7 +109,8 @@
                             <label for="matric_no" class="col-lg-3 col-form-label form-control-label">{{ __('Skills') }}</label>
 
                             <div class="col-lg-9">
-                                <input id="skills" type="textarea" class="form-control @error('skills') is-invalid @enderror" name="skills" >
+                                <input id="skills" type="textarea" class="form-control @error('skills') is-invalid @enderror" name="skills" 
+                                value="{{ old('skills') ?? $user->profile->skills }}">
 
                                 @error('skills')
                                     <span class="invalid-feedback" role="alert">
@@ -137,6 +119,7 @@
                                 @enderror
                             </div>
                         </div>
+                    </div>
 
                         
 
@@ -166,16 +149,38 @@
 
                 </div>
             </div>
+            <br><br>
+        </div>                
+    
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header"> My Profile</div>
+                <div class="card-body">
+                    <p><b>Name: </b>{{ Auth::user()->name }}</p>
+                    <p><b>Email: </b>{{ Auth::user()->email }}</p>
+                    <p><b>Matric No.: </b>{{ $user->profile->matric ?? 'N/A' }}</p>
+                    <p><b>Kulliyyah: </b>{{ $user->profile->kulliyyah ?? 'N/A'}}</p>
+                    <p><b>Level: </b>{{ $user->profile->level ?? 'N/A'}}</p>
+                    <p><b>Phone No.: </b>{{ $user->profile->phone ?? 'N/A'}}</p>
+                    <p><b>Skills: </b>{{ $user->profile->skills ?? 'N/A'}}</p>
+
+                </div>
+            </div>
+            <br>
+            <div class="card">
+                <div class="card-header">Profile Picture</div>
+                <div class="card-body">
+                    <input type="file" name="profile_picture" class="form-control">
+                    <br>
+                </div>
+                
+            </div>
+        </div>
+         
         </div>
 
-
-
-
-                    
-        </div>
     </div>
 
-</div>
 
                    
 @endsection
