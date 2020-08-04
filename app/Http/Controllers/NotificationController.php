6eba60347678c6f46bb601a23e4b768d2074a4bb
ainmohamad <47972834+ainmohamad@users.noreply.github.com>
2020-08-04 20:51:17 +0800

    Update NotificationController.php

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Notifications\EventReminder;
+use App\Notifications\SuccessfullyApplyforEvent;
 use Illuminate\Support\Facades\Notification;
+use App\User;
 
 class NotificationController extends Controller
 {
+    public function index(){
+    	return view('Student.Notifications');
+    }
     
     //send notification to current user
     public function store(){
@@ -18,6 +22,21 @@ class NotificationController extends Controller
 
     	return redirect()->back()->with('status', 'Reminder has been set successfuly!');
 
+    }
+
+    public function sendNotification(){
+        session()->flash('Successmessage', 'You have successfully applied for this event. You can check the status of your application in the dashboard.');
+
+        $user = Auth::user();
+        $user->notify(new SuccessfullyApplyforEvent(User::findOrFail(1)));
+        return view('Student.StudentDashboard');    
+
+
+    }
+
+    public function notifications()
+    {
+        return auth()->user()->unreadNotifications()->limit(5)->get()->toArray();
     }
 
 
