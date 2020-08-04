b5a57340a9765bb28a2be41e87849dc9d3ca5cc7
ainmohamad <47972834+ainmohamad@users.noreply.github.com>
2020-08-04 20:30:42 +0800

    Create footer.blade.php

---
+++ resources/views/vendor/mail/html/footer.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td>
+<table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td class="content-cell" align="center">
+<small> &copy; 2020 Committee Manager. All rights reserved.</small>
+</td>
+</tr>
+</table>
+</td>
+</tr>
