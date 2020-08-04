7c80c1b4a39a9d5d46f6b5a28313994ee10c65f0
ainmohamad <47972834+ainmohamad@users.noreply.github.com>
2020-08-05 07:53:17 +0800

    Create Notifications.blade.php

---
+++ resources/views/Student/Notifications.blade.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+	<h4>Notifications</h4>
+</div>
+@endsection
